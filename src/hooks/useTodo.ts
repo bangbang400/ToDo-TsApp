7c6e0419a6a4e41f222d7f342ec308181521914d
bangbang400 <bangbang400cc@gmail.com>
2023-02-02 22:20:50 +0900

    add:サンプルコード記述完了　動作確認は未実施

--- src/hooks/useTodo.ts
+++ src/hooks/useTodo.ts
@@ -3,6 +3,7 @@ import { ulid } from "ulid";
 
 import * as todoData from "../apis/todos";
 import { Todo } from "../types/Todo";
+import { deleteTodoData } from '../apis/todos';
 
 export const useTodo = () => {
     const [todoList,setTodoList] = useState<Todo[]>([]);
@@ -13,4 +14,32 @@ export const useTodo = () => {
             setTodoList([...todo].reverse);
         });
     }, []);
-}
+
+    const toggleTodoListItemStatus = (id: string, done: boolean) => {
+        const todoItem = todoList.find((item: Todo) => item.id === id);
+
+        const newTodoItem: Todo = {...todoItem!, done: !done};
+        
+        todoData.updateTodoData(id, newTodoItem).then((updatedTodo) => {
+            const newTodoList = todoList.map((item) => (item.id !== updatedTodo.id ? item: updatedTodo));
+            setTodoList(newTodoList);
+        });
+    };
+
+    const addTodoListItem = (todoContent: string) => {
+        const newTodoItem = {id: ulid(), content: todoContent, done: false};
+
+        todoData.deleteTodoData(newTodoItem).then((addTodo) => {
+            setTodoList([addTodo, ...todoList]);
+        });
+    };
+
+    const deleteTodoListItem = (id: string) => {
+        totoData.deleteTodoData(id).then((deletedid) => {
+            const newTodoList = todoList.filter((item) => item.id !== deletedid);
+            setTodoList(newTodoList);
+        });
+    };
+
+    return { todoList, toggleTodoListItemStatus, addTodoListItem, deleteTodoListItem};
+};
